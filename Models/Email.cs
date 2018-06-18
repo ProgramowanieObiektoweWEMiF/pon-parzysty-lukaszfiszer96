00229290c7988f386a560a65393a1ba3d9120280
Lukasz <wkret96pl@gmail.com>
2018-06-18 15:31:27 +0200

    Add files via upload

---
+++ Models/Email.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PO_Messenger.Models
+{   
+    [Serializable]
+    public class Email : Message
+    {
+
+        public string Topic { get; set; }
+        public string RecipientEmial { get; set; }
+        private string messageType = "Email";
+
+        public string MessageType
+        {
+            get { return messageType; }
+
+        }
+
+       
+    }
+}
---
+++ Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PO_Messenger.Models
+{   [Serializable]
+    public class Message
+    {
+        public string Contents { get; set; }
+        public string Recipient { get; set; }
+    }
+}
