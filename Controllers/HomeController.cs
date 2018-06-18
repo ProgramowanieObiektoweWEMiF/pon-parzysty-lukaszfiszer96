4ce9aed806a79d490bbff391b192a37cf20ed3ff
Lukasz <wkret96pl@gmail.com>
2018-06-18 15:28:28 +0200

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PO_Messenger.Models;
+using System.Xml;
+namespace PO_Messenger.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+        
+        [HttpGet]
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(Users obj)
+
+        {
+            if (ModelState.IsValid)
+            {
+                UsersEntities db = new UsersEntities();
+                if (db.Users.Any(p => p.EmialAdress.Equals(obj.EmialAdress)))
+                {
+                    ModelState.AddModelError(string.Empty, "Email allready exist");
+                    return View();
+                }
+                else
+                {
+                    db.Users.Add(obj);
+                    db.SaveChanges();
+                    return View("UserAdded", obj);
+                }
+            }
+            else
+                return View();                    
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(Users obj)
+        {
+            using(UsersEntities db = new UsersEntities())
+            {
+                var userDetails = db.Users.Where(x => x.EmialAdress == obj.EmialAdress && x.Password == obj.Password).FirstOrDefault();
+
+                if (userDetails == null)
+                    return View();
+                else
+                {
+                    Session["userName"] = userDetails.Name;
+                    Session["userObj"] = userDetails;
+
+                    return RedirectToAction("UserMain", "Loguser");
+                }
+            }
+                
+        }
+
+
+
+    }
+
+
+}
---
+++ Controllers/LoguserController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PO_Messenger.Models;
+using System.Xml;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+
+namespace PO_Messenger.Controllers
+{
+    public class LoguserController : Controller
+    {
+        // GET: Loguser
+        public ActionResult UserMain()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult SendEmail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendEmail(Email email_obj)
+        {
+
+            FileOperationWritter(email_obj);
+            return View("MessageSent");
+        }
+
+        public ActionResult CheckHistory()
+        {
+
+            FileOperationReader();
+            return View();
+        }
+
+        public void FileOperationWritter(Email email_obj)
+        {
+            string name = (String)Session["userName"];
+            FileStream fs = new FileStream("C:\\Users\\wkret\\source\\repos\\PO_Messenger\\PO_Messenger\\Uzytkownicy\\"+name+".txt",
+               FileMode.Append, FileAccess.Write);
+
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(email_obj.Contents);
+            sw.WriteLine(email_obj.RecipientEmial);
+            sw.WriteLine(email_obj.MessageType);
+            sw.Close();
+        }
+        public void FileOperationReader()
+        {
+            string name = (String)Session["userName"];
+
+            try
+            {
+                FileStream fs = new FileStream("C:\\Users\\wkret\\source\\repos\\PO_Messenger\\PO_Messenger\\Uzytkownicy\\" + name + ".txt",
+                    FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+
+                string DataFromFile = sr.ReadToEnd();
+                sr.Close();
+
+                ViewBag.FileData = DataFromFile;
+            }
+            catch(Exception ex)
+            {
+                ViewBag.FileData = ex.ToString();
+            }
+
+        }
+    }
+}
